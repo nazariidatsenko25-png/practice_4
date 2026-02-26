59bcef31cf33242a7f4a0911747dd146f897a804
admin <nazarii.datsenko.25@kse.org.ua>
2026-02-26 18:58:23 +0200

    added student class

--- assignment4.cs
+++ assignment4.cs
@@ -18,6 +18,37 @@ public class Course
     {
         return $"Course ID: {Id}, Name: {Name}, Teacher: {Teacher}";
     }
+}
+
+public class Student
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
     
+    private List<Course> EnrolledCourses = new();
+
+    public Student(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    
+    public void AddCourse(Course course)
+    {
+        EnrolledCourses.Add(course);
+    }
+
+    public void ShowCourses()
+    {
+        Console.WriteLine($"ваші курси, {Name}:");
+        forech(var course in EnrolledCourses)
+        {
+            Console.WriteLine(course.GetInfo());
+        }
+    }
+    public string GetInfo()
+    {
+        return $"{Name} ID: {Id}, зарахований до таких курсів: {EnrolledCourses.Count} .";
+    }
 }
 
